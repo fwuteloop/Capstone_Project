1cd00794ffeaa0aad152731692bd9e9f2c820cdd
Wynta Carter <46628519+fwuteloop@users.noreply.github.com>
2022-10-10 11:16:00 -0400

    Added and tweaked enemy pathways

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    WaveManager waveManager;
+    UIManager uiManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiManager = GameObject.FindObjectOfType<UIManager>();
+        waveManager = GameObject.FindObjectOfType<WaveManager>();
+
     }
 
     // Update is called once per frame
